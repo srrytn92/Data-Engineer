a9cf24106e75fb5a93eb9c193d5ab2c610babcd1
Siriroj Yolthanom <83905993+srrytn92@users.noreply.github.com>
2023-12-23 13:53:34 +0700

    Add files via upload

---
+++ CH 3 Basic Cloud & Workshop 3/bash.sh
@@ -0,0 +1,29 @@
+ls list file
+ls -l
+ls -lh
+ls -la
+ls -lah
+pwd print working directory
+cd ~ change derectory
+echo "text"
+echo "text" >> [file name]
+cat [file name]
+more [file name]
+less [file name]
+touch [file name]
+mkdir [directory name]
+mkdir -p new_folder/in/folder
+cp [source] [destination]
+cp -r [source] [destination]
+mv [source] [destination]
+rm [file name]
+rm -r [directory]
+wget [-O output name] [url]
+ls *.zip
+unzip [zip file]
+zip [zip file] [file]...                 
+wc [file name]
+wc -l [file name]
+man [command]
+vim [file name]                               
+nano [file name]
